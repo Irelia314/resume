b67fdaf86bad9704c419339d4e24aca27b3d8e84
huangfangjie <67227184+Irelia314@users.noreply.github.com>
2026-08-03 15:28:36 +0800

    Conditionally render Wrapper for about me section

--- src/components/Resume/Template2/index.tsx
+++ src/components/Resume/Template2/index.tsx
@@ -223,15 +223,17 @@ export const Template2: React.FC<Props> = props => {
             })}
           </Wrapper>
         ) : null}
-        <Wrapper
-          title={<FormattedMessage id="自我介绍" />}
-          className="section section-aboutme"
-          color={theme.color}
-        >
-          {aboutme.map((d, idx) => (
-            <div key={`${idx}`}>{d}</div>
-          ))}
-        </Wrapper>
+        {!!_.trim(_.join(aboutme, '')) && (
+          <Wrapper
+            title={<FormattedMessage id="自我介绍" />}
+            className="section section-aboutme"
+            color={theme.color}
+          >
+            {aboutme.map((d, idx) => (
+              <div key={`${idx}`}>{d}</div>
+            ))}
+          </Wrapper>
+        )}
         {/* 专业技能 */}
         {skillList?.length ? (
           <Wrapper
